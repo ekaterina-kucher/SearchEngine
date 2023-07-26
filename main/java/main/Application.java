057e189e9e4bd48912f15fc8e48fe5d34864101a
ekaterina-kucher <ekaterina-kucher@mail.ru>
2023-07-27 01:05:23 +0300

    Initial commit

---
+++ main/java/main/Application.java
@@ -0,0 +1,20 @@
+package main;
+
+import main.repository.Repos;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean(initMethod="init")
+	public Repos getBean() {
+		return new Repos();
+	}
+}
